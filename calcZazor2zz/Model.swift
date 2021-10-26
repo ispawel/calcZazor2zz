b89e0fe7722c40deab73fb520a7d2b554a34a38e
Pavel Isakov <81164077+ispawel@users.noreply.github.com>
2021-10-27 00:57:56 +0700

    Create App

---
+++ calcZazor2zz/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  calcZazor2zz
+//
+//  Created by Pavel Isakov on 26.10.2021.
+//
+
+import Foundation
+
+struct
--- calcZazor2zz/ViewController.swift
+++ calcZazor2zz/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+
+    @IBOutlet weak var inZazor: UITextField!
+    
+    @IBOutlet weak var inOldShaiba: UITextField!
+    
+    @IBOutlet weak var inNewShaibaLabel: UILabel!
+    
+    
+    @IBOutlet weak var outZazor: UITextField!
+    @IBOutlet weak var outOldShaiba: UITextField!
+    
+    @IBOutlet weak var outNewShaibaLabel: UILabel!
+
+
+    func calcInShaiba(inZazor:Double, inOldShaiba:Double) -> Double{
+        let inNewShaiba = inOldShaiba + (inZazor - 0.13) * 1.5
+        inNewShaibaLabel.text = "\(inNewShaiba)"
+        return inNewShaiba
     }
-
-
+    func calcOutShaiba(outZazor:Double, outOldShaiba:Double) -> Double{
+        let outNewShaiba = outOldShaiba + (outZazor - 0.27) * 1.5
+        outNewShaibaLabel.text = "\(outNewShaiba)"
+        return outNewShaiba
+    }
+  
+ 
+    @IBAction func button1(_ sender: UIButton) {
+        let inZazor = Double(inZazor.text!)
+        let inOldShaiba = Double(inOldShaiba.text!)
+        calcInShaiba(inZazor: inZazor!, inOldShaiba: inOldShaiba!)
+    }
+     
+    @IBAction func button2(_ sender: UIButton) {
+        let outZazor = Double(outZazor.text!)
+        let outOldShaiba = Double(outOldShaiba.text!)
+        calcOutShaiba(outZazor: outZazor!, outOldShaiba: outOldShaiba!)
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-
